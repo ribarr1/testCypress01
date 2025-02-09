495b7a1a4fe601bb28805b42345e27ef9dc09935
greensqa2021 <ribarra@greensqa.com>
2025-02-09 13:02:30 -0500

    actualizacion del cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    //baseUrl: 'http://localhost:3000', 
-    specPattern: 'cypress/e2e/**/*.spec.{js,ts}', 
-    supportFile: 'cypress/support/e2e.ts', 
+    //baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://practicetestautomation.com/',
+    specPattern: 'cypress/e2e/*/.spec.{js,ts}', 
+    supportFile: 'cypress/support/e2e.ts',
+    viewportWidth: 1280,
+    viewportHeight: 800,
+    defaultCommandTimeout: 5000,
   },
   reporter: "mochawesome",
   reporterOptions: {

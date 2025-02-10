a5c0f329c8df5a083e02a2d994ccf637b4b68ccd
ribarr1@gmail.com <ribarr1@gmail.com>
2025-02-09 22:17:46 -0500

    update test case convenio process

--- cypress/e2e/ui/conveniosAval.spec.ts
+++ cypress/e2e/ui/conveniosAval.spec.ts
@@ -2,9 +2,21 @@ describe('Convenios Aval process', () => {
     it('process convenios cancelling pasarela', () => {
       cy.visit('https://piloto.avalpaycenter.com/wps/portal/portal-de-pagos/web/pagos-aval/'); 
       cy.get('#search-input').type('Auteco Mobility Sas');
-      cy.get('.btn-primary').click();
+      cy.get('button.btn-primary').first().click({ force: true });
       
       //cy.url().should('include', 'logged-in-successfully/');
-      cy.get('name_convenio', { timeout: 5000 }).should('be.visible').and('have.text', 'Auteco Mobility Sas'); 
+      cy.get('.title').should('contain.text', 'Auteco mobility sas');
+      cy.contains('button', 'Pagar').click();
+
+      cy.contains('.name_convenio', 'Auteco Mobility Sas').should('be.visible');
+      cy.get('input[id="cedula o nit"]').type('123456789');
+      cy.get('input[id="valor a pagar"]').type('235');
+      cy.get('label.slider').click();
+      cy.get('textarea#detallePago').type('Payment detail');
+      cy.get('.checkbox-container input[type="checkbox"]').check();
+      cy.contains('button', 'Pagar').click();
+
+      cy.contains('button', 'Aceptar').click();
+ 
     });
-  });
+  }); 
